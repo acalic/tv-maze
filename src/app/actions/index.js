a33329d73b8e6f987f59a87892b29175dc6d14e8
acalic <acalic1990@gmail.com>
2022-01-07 00:04:06 +0000

    Updated search action and fixed a warning

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -39,33 +39,25 @@ export const toggleLocalTimezone = () => {
   };
 };
 
-/* export const searchShows = (payload) => {
-  return function (dispatch) {
-    dispatch({
-      type: "SEARCH_SHOWS",
-      payload,
-    });
-  };
-}; */
-
 export const searchShows = (payload) => {
-  try {
-    return async (dispatch) => {
-      const result = await fetch(`${apiUrl}/search/shows?q=${payload}`);
-      const json = await result.json();
-      if (json) {
-        dispatch({
-          type: "SEARCH_SHOWS",
-          payload: {
-            searchKeyword: payload,
-            searchResults: json,
-          },
-        });
-      } else {
-        console.log("Unable to fetch!");
-      }
-    };
-  } catch (error) {
-    console.log(error);
-  }
+  return async (dispatch) => {
+    fetch(`${apiUrl}/search/shows?q=${payload}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.length) {
+          dispatch({
+            type: "SEARCH_SHOWS",
+            payload: {
+              searchKeyword: payload,
+              searchResults: data,
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 };
